c002a6670e342d8ecef819a0dca32cfd900b4dcd
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-27 22:24:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,6 @@
 //     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 //   }
 // }
+BoardMember.prototype.veto = function() {
+  return 'No, I must disagree'
+}
